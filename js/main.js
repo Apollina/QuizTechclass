782a1f70a4eacc7853576d3e537f642e238380cc
Apollinariia <apollinariia.gainulenko@metropolia.fi>
2017-05-29 03:38:51 +0300

    timer, progress bar, results

--- js/main.js
+++ js/main.js
@@ -8,6 +8,78 @@ $(function(){
         loading.hide();
     });
 
+    /*-- timer --*/
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        //var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+            'total': t,
+            //'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function initializeClock(id, endtime) {
+        var clock = document.getElementById(id);
+        //var daysSpan = clock.querySelector('.days');
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
+
+        function updateClock() {
+            var t = getTimeRemaining(endtime);
+
+            //daysSpan.innerHTML = t.days;
+            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+            //What happens when the time is up
+            if (t.total <= 0) {
+                clearInterval(timeinterval);
+                // Get the modal
+                var modal = document.getElementById('myModal');
+
+                // Get the <span> element that closes the modal
+                var span = document.getElementsByClassName("close")[0];
+
+                // Show modal
+                    modal.style.display = "block";
+
+                // When the user clicks on <span> (x), close the modal
+                span.onclick = function() {
+                    modal.style.display = "none";
+                    window.location="index.html";
+                }
+
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function(event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                        window.location="index.html";
+                    }
+                }
+
+            }
+        }
+
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+    }
+
+    var deadline = new Date(Date.parse(new Date()) + 2 * 60 * 60 * 1000);
+    initializeClock('clockdiv', deadline);
+
+    /*-- /timer --*/
+
+
+
+    /*-- quiz --*/
     var questionNo = 0;
     var correctCount = 0;
     var q = [
@@ -41,9 +113,16 @@ $(function(){
         ink.css({top: y+'px', left: x+'px'}).addClass("animate");
         //ripple end
 
+        //progress bar
+        var bar = document.getElementsByClassName("progress-bar");
+        var width = 1;
+        var perPercent = (width/q.length) * 100;
+        bar[0].style.width = perPercent * (questionNo + 1 ) + '%';
+        //progress bar
+
         var choice = $(this).parent().find('input:radio').val();
         console.log(choice);
-        var anscheck =  $(this).checking(questionNo, choice);//$( "#answer" ).html(  );
+        var anscheck =  $(this).checking(questionNo, choice);
         q[questionNo].UC = choice;
         if(anscheck){
             correctCount++;
@@ -52,17 +131,22 @@ $(function(){
             q[questionNo].result = "Incorrect";
         }
         console.log("CorrectCount:" + correctCount);
+
         setTimeout(function(){
             $('#loadbar').show();
             $('#quiz').fadeOut();
             questionNo++;
             if((questionNo + 1) > q.length){
-                alert("Quiz completed, Now click ok to get your answer");
-                $('label.element-animation').unbind('click');
+                $.alert({
+                    title: 'Congratulations!',
+                    content: 'You have completed the quiz.'
+                });
+
                 setTimeout(function(){
+                    document.getElementById("clockdiv").style.display = "none";
                     var toAppend = '';
                     $.each(q, function(i, a){
-                        toAppend += '<tr>'
+                        toAppend += '<tr>';
                         toAppend += '<td>'+(i+1)+'</td>';
                         toAppend += '<td>'+a.A+'</td>';
                         toAppend += '<td>'+a.UC+'</td>';
@@ -70,7 +154,14 @@ $(function(){
                         toAppend += '</tr>'
                     });
                     $('#quizResult').html(toAppend);
-                    $('#totalCorrect').html("Total correct: " + correctCount);
+                    $('#totalCorrect').html("Your score is: " + correctCount + "/" + q.length);
+                    if (correctCount >= (70 * q.length)/100) {
+                        $('#pass').html("You passed the quiz");
+                        document.getElementById("fail").style.display = "none";
+                    } else {
+                        $('#fail').html("You failed the quiz. You can re-take the quiz later.");
+                        document.getElementById("pass").style.display = "none";
+                    }
                     $('#quizResult').show();
                     $('#loadbar').fadeOut();
                     $('#result-of-question').show();
@@ -102,6 +193,6 @@ $(function(){
             return true;
     };
 
-    
+
 });
 
